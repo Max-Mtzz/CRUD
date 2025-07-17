7d10bd28e6f792c7e85364e3ecb8cebf1b4eae77
Sam6482 <20243ds141utez.edu.mx>
2025-07-17 08:43:16 -0600

    Test de conexion exitoso amores

---
+++ src/main/java/com/jmc/crud/TestConexion.java
@@ -0,0 +1,13 @@
+package com.jmc.crud;
+import java.sql.Connection;
+
+public class TestConexion {
+    public static void main(String[] args) {
+        try (Connection con = conexion.conectar()) {
+            System.out.println("¡Conexión exitosa a Oracle!");
+        } catch (Exception e) {
+            System.out.println("Error de conexión:");
+            e.printStackTrace();
+        }
+    }
+}

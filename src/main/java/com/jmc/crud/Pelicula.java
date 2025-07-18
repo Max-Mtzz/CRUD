96824762988cecf45cea3379ad2a30499a922718
JNMC0 <20243ds126@utez.edu.mx>
2025-07-18 13:34:45 -0600

    agregue los constructores el bob

--- src/main/java/com/jmc/crud/Pelicula.java
+++ src/main/java/com/jmc/crud/Pelicula.java
@@ -2,6 +2,14 @@ package com.jmc.crud;
 
 public class Pelicula {
     private int id;
+
+    public Pelicula(int id, String nombre, String genero, int ano) {
+        this.id = id;
+        this.nombre = nombre;
+        this.genero = genero;
+        this.ano = ano;
+    }
+
     private String nombre;
     private String genero;
     private int ano;

b758188f278ec06712ff780567df15f0d7e5fb9a
Max-Mtzz <20243ds119@utez.edu.mx>
2025-07-18 13:32:10 -0600

    Inicialice las variables y la lista que mostrara los datos

--- src/main/java/com/jmc/crud/HelloController.java
+++ src/main/java/com/jmc/crud/HelloController.java
@@ -9,8 +9,19 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import java.sql.*;
 
 
 public class HelloController {
+    @FXML private TextField txtId, txtNombre, txtAno;
+    @FXML private SplitMenuButton generoMenu;
+    @FXML private TableView<Pelicula> tablePeliculas;
+    @FXML private TableColumn<Pelicula, Integer> colId;
+    @FXML private TableColumn<Pelicula, String> colNombre;
+    @FXML private TableColumn<Pelicula, String> colGenero;
+    @FXML private TableColumn<Pelicula, Integer> colAno;
+
+    private ObservableList<Pelicula> listaPeliculas = FXCollections.observableArrayList();
+    private String generoSeleccionado = "";
 
 }

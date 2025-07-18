df9c4b1837a64f76ed5c43f8ccf04f897bea81c8
JNMC0 <20243ds126@utez.edu.mx>
2025-07-18 13:35:35 -0600

    agregue setter and geys

--- src/main/java/com/jmc/crud/Pelicula.java
+++ src/main/java/com/jmc/crud/Pelicula.java
@@ -3,6 +3,38 @@ package com.jmc.crud;
 public class Pelicula {
     private int id;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
     public Pelicula(int id, String nombre, String genero, int ano) {
         this.id = id;
         this.nombre = nombre;

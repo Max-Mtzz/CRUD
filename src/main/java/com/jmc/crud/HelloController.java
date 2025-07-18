bd6fecd8c4df212a1b085d704cb9761a90497c67
Max-Mtzz <20243ds119@utez.edu.mx>
2025-07-18 13:26:07 -0600

    Importe las clases pelicula y conexion al controllador

--- src/main/java/com/jmc/crud/HelloController.java
+++ src/main/java/com/jmc/crud/HelloController.java
@@ -2,13 +2,15 @@ package com.jmc.crud;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import com.jmc.crud.conexion;
+import com.jmc.crud.Pelicula;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
